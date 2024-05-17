aeb853c24ed4bd32f9d1541982d2bebb0ee992b4
Ribesh <ribeshpokharel@gmail.com>
2024-05-17 13:30:18 +1000

    Final Commit

--- app/src/main/java/com/example/chatbot/Message.java
+++ app/src/main/java/com/example/chatbot/Message.java
@@ -1,27 +1,27 @@
 package com.example.chatbot;
 
 public class Message {
-    private String content;
-    private boolean isUser; // true if the message is from the user, false if from the bot
+    private String User;
+    private boolean Llama; // true if the message is from the user, false if from the bot
 
     public Message(String content, boolean isUser) {
-        this.content = content;
-        this.isUser = isUser;
+        this.User = content;
+        this.Llama = isUser;
     }
 
     public String getContent() {
-        return content;
+        return User;
     }
 
     public void setContent(String content) {
-        this.content = content;
+        this.User = content;
     }
 
     public boolean isUser() {
-        return isUser;
+        return Llama;
     }
 
     public void setUser(boolean user) {
-        isUser = user;
+        Llama = user;
     }
 }
